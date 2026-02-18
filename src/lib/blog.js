6b58a333d1dc27e49515406443a4bb2e357c589a
sradigales <sradigales.clb@grupoprisa.com>
2026-02-18 12:45:54 +0100

    refactor blog entries to use anchor tags and improve styling

--- src/lib/blog.js
+++ src/lib/blog.js
@@ -4,16 +4,22 @@ const entries = [
     tags: ["Design Patterns", "Software Architecture"],
     shortDescription:
       "Factory Method is a creational design pattern that defines an interface for creating an object, but lets subclasses decide which concrete class to instantiate.",
+    route: "./../pages/blog-entries/factory-method.html",
   },
 ];
 
 const blogContent = document.querySelector(".blog-content");
 
 entries.forEach((entrie) => {
-  const entryContainer = document.createElement("div");
+  const entryContainer = document.createElement("a");
+
+  entryContainer.href = entrie.route;
 
   const entrieTitle = document.createElement("span");
   entrieTitle.textContent = `Title: ${entrie.title}`;
+  Object.assign(entrieTitle.style, {
+    fontWeight: "bold",
+  });
 
   const entrieShortDescription = document.createElement("span");
   entrieShortDescription.textContent = `Description: ${entrie.shortDescription}`;
@@ -22,11 +28,10 @@ entries.forEach((entrie) => {
   entryContainer.append(entrieShortDescription);
 
   entrie.tags.forEach((tag) => {
-    console.log(tag);
     const tagContaier = document.createElement("div");
 
     Object.assign(tagContaier.style, {
-      border: "2px solid white",
+      border: "2px solid rgb(235, 230, 192)",
       borderRadius: "10px",
       width: "fit-content",
       padding: "0.5rem",
@@ -41,12 +46,13 @@ entries.forEach((entrie) => {
 
   Object.assign(entryContainer.style, {
     width: "20rem",
-    border: "2px solid white",
+    border: "2px solid rgb(235, 230, 192)",
     borderRadius: "10px",
     padding: "1rem",
     display: "flex",
     flexDirection: "column",
     gap: "1rem",
+    textDecoration: "none",
   });
 
   blogContent.append(entryContainer);
